685146517792c2f96cd7fa18f38acbec0babc71a
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-08-03 17:54:06 -0700

    deployment config

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    PORT: 3000,
+    PORT: process.env.PORT || "3000",
     MONGO_URI: process.env.MONGO_URI,
     SECRET: process.env.SECRET,
     SALT: 10
